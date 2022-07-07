cd71a20f34ecbf369f589f5b01d9e389a92982b0
Fire-45 <77906966+Fire-45@users.noreply.github.com>
2022-07-07 10:58:15 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
  song = "";
+ leftWristX = 0;
+ leftWristY = 0;
+ rightWristX = 0;
+ rightWristY = 0;
+
 
 function preload(){
 
-    song = loadSound("music.mp3")
+   song = loadSound("music.mp3")
  }
  
 
@@ -11,12 +16,68 @@ function setup(){
 canvas = createCanvas(600, 500);
 canvas.position(250,190);
 video = createCapture(VIDEO)
+video.size(600,500);
 video.hide();
+
+posenet  = ml5.poseNet(video,modelLoaded);
+posenet.on("pose",gotPoses);
 }
+
+function modelLoaded(){
+
+    console.log("model loaded");
+}
+
+function gotPoses(result){
+    
+    console.log(result);
+
+    leftWristX = result[0].pose.leftWrist.x;
+    leftWristY = result[0].pose.leftWrist.y;
+    rightWristX = result[0].pose.rightWrist.x;
+    rightWristY = result[0].pose.rightWrist.y;
+
+    }
 
 function draw(){
 
     image(video, 0, 0, 600, 500);
+
+    fill("red");
+    circle(leftWristX,leftWristY,40);
+    circle(rightWristX,rightWristY,40);
+    
+    convertNumber = Number(leftWristY);
+    removeDecimal = floor(convertNumber);
+    volume = removeDecimal / 500;
+    document.getElementById("volumeValue").innerHTML = volume;
+    song.setVolume(volume);
+
+    if(rightWristY > 0 && rightWristY < 100){
+        document.getElementById("speedValue").innerHTML = "0.5 X ";
+        song.rate(0.5);
+
+    }
+
+    if(rightWristY > 100 && rightWristY < 200){
+        document.getElementById("speedValue").innerHTML = "1.0";
+        song.rate(1.0);
+    }
+
+    if(rightWristY > 200 && rightWristY < 300){
+        document.getElementById("speedValue").innerHTML = "1.5";
+        song.rate(1.5);
+    }
+
+    if(rightWristY > 300 && rightWristY < 400){
+        document.getElementById("speedValue").innerHTML = "2.0";
+        song.rate(2.0);
+    }
+
+    if(rightWristY > 400 && rightWristY < 500){
+        document.getElementById("speedValue").innerHTML = "2.5";
+        song.rate(2.5);
+    }
 }
 
 
@@ -25,5 +86,7 @@ function draw(){
 
 
 function playSong(){
-    //song.play();
+    song.setVolume(1);
+    song.rate(1)
+    song.play();
 }
